c3067f981d20d9603537da08e234eb68d757af6e
Nchansard <chansardnathanael@gmail.com>
2023-01-04 11:36:46 +0100

    déclaration de la classe joueur-et-de

---
+++ De.h
@@ -0,0 +1,12 @@
+#ifndef DE_H
+#define DE_H
+
+#include <iostream>
+
+class De
+{
+  private:
+    unsigned int valeur;
+    unsigned int nbFaces;
+}
+#endif // De_H
---
+++ Joueur.h
@@ -0,0 +1,13 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include <iostream>
+
+class Joueur
+{
+  private:
+    string       nom;
+    unsigned int score;
+    bool         aGagne;
+}
+#endif // JOUEUR_H
