034a19770b245d723b22e26ed91cbd1d9a1c65e7
NChansard <100561385+NChansard@users.noreply.github.com>
2023-01-04 14:32:32 +0100

    Update Joueur.h

--- Joueur.h
+++ Joueur.h
@@ -10,15 +10,13 @@ class Joueur
   private:
     std::string  nom;
     unsigned int score;
-    bool         aGagne;
 
   public:
     Joueur(std::string nom = "");
-    string       getNom() const;
+    std::string       getNom() const;
     unsigned int getScore() const;
     void         setNom(std::string nom);
     void         setScore(unsigned int score);
-    bool         aGagne() const;
 }
 
 #endif // JOUEUR_H
