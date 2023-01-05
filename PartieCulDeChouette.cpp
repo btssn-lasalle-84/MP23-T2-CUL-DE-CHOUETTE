d32b7d70670bf7c7bf52f29e63f54d91087e955c
Async-Mayeul <mayeulfargier10@gmail.com>
2023-01-05 13:59:38 +0100

    MP23T2-18 suppresion de l'énumération Regle et des méthodes associé dans la classe PartieCulDeChouette

--- PartieCulDeChouette.cpp
+++ PartieCulDeChouette.cpp
@@ -10,7 +10,7 @@ using namespace std;
 PartieCulDeChouette* PartieCulDeChouette::instance = nullptr;
 
 PartieCulDeChouette::PartieCulDeChouette(unsigned int nbJoueurs) :
-    nbJoueurs(nbJoueurs), regle(REGLE_DEFAUT)
+    nbJoueurs(nbJoueurs)
 {
     for(int i = 0; i < NB_DES; i++)
     {
@@ -57,13 +57,7 @@ void PartieCulDeChouette::afficherPartie() const
         cout << "Score " << this->joueurs[i].getNom() << " : "
              << to_string(this->joueurs[i].getScore()) << endl;
     }
-    cout << "Règle utilisée : " << this->regle << endl;
     cout << setfill('#') << setw(30) << endl;
-}
-
-void PartieCulDeChouette::choisirRegles(Regle regle)
-{
-    this->regle = regle;
 }
 
 PartieCulDeChouette* PartieCulDeChouette::getInstance(unsigned int nbJoueurs)
@@ -83,29 +77,6 @@ void PartieCulDeChouette::detruireInstance()
 
 void PartieCulDeChouette::lancerPartie()
 {
-    int   numeroRegle;
-    Regle regle;
-
-    cout
-      << "Choisir une règle Chouette (1) | Velute (2) | Cul De Chouette (3) :";
-    cin >> numeroRegle;
-
-    switch(numeroRegle)
-    {
-        case 1:
-            regle = Chouette;
-            break;
-        case 2:
-            regle = Velute;
-            break;
-        case 3:
-            regle = CulDeChouette;
-            break;
-        default:
-            regle = REGLE_DEFAUT;
-    }
-
-    instance->choisirRegles(regle);
     instance->setDes();
     instance->saisirNoms();
     instance->afficherPartie();
